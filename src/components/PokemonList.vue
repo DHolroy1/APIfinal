37109dde02bdf046a7610bee9f742aaddb31d3f6
CodeLancashire Learner 2 <darren.holroyd@live.co.uk>
2023-03-16 18:49:40 +0000

    Final assesment

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -8,5 +8,22 @@
 // import pokemons from "@/assets/sampledataset.json"
 import Pokemon from "./Pokemon.vue"
 
+import {ref, onMounted} from "vue"
+
+import client from "@/assets/apiclient.js"
+
 // mount and then make api call
+
+const pokemons = ref([])
+
+onMounted(()=>{
+    client.get("/pokemon").then(res => {
+        console.log("response received", res.data)
+        pokemons.value = res.data
+}).catch(errors =>{
+    console.log("error", errors)
+        })
+    })
+
+
 </script>
